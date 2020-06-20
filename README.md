3c13a764417776e4b5e7f55ee4df0e91c25a207e
bnigatu <bizuayehuwond@yahoo.com>
2020-06-20 11:13:01 -0600

    environment preparation script was added

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # UNIX-Fundamentals
+***
 This course examines the basic features of Unix OS, UNIX commands, the UNIX file system, the UNIX shells and shell programming, and comparisons between UNIX and Linux. In this course, we examine the interaction of end Users with the UNIX operating system.
+
+### Download and Setup
+
+The course scripts were designed to run on  [Ubuntu](https://ubuntu.com/) 16.04 or above.
+It was also tested on  [CentOs](https://centos.org/) 7.0 or above and [MacOs](https://apple.com) Sierra and above.
+Download the course matterials as follows.
+
+```sh
+$ wget https://github.com/bnigatu/envprep.sh -P /tmp
+$ chmod u+x /tmp/envprep.sh
+$ /tmp/envprep.sh
+```
---
+++ envprep.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# author	: Dr. Nigatu
+# date  	: 06-20-2020
+# description	: Prepares environment needed for Unix Fundamental Course
+# file name	: envprep.sh
+
+
+# collect user name and password
+read -p "Would you like the shell to stop asking for new password? (y/n): " answer
+case ${answer:0:1} in
+    y|Y )
+        sudo bash -c 'echo "$(logname) ALL=(ALL:ALL) NOPASSWD: ALL" | (EDITOR="tee -a" visudo)'
+    ;;
+    * )        
+    ;;
+esac
+
+# install necessary applications
+sudo apt-get install snap vim git -y
+
+# install visualcode
+sudo snap install --classic code
+
+# fix arrow key probelm with vi
+echo "set nocompatible" > ~/.vimrc
+
+# download course files from git hub
+unset answer
+read -p "Would you like to download course materials? (y/n): " answer
+case ${answer:0:1} in
+    y|Y )
+        git clone git@github.com:bnigatu/unix-fundamentals.git ~/unix-fundamentals
+    ;;
+    * )        
+    ;;
+esac
+
